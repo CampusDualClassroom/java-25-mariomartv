ce9067f8704cbaa2e560312db70da4d11cca782a
Mario <marionuman@gmail.com>
2024-09-27 13:19:17 +0200

    Ejercicio 25 Completado

--- src/main/java/com/campusdual/classroom/Exercise25.java
+++ src/main/java/com/campusdual/classroom/Exercise25.java
@@ -4,11 +4,19 @@ import java.util.Stack;
 
 public class Exercise25 {
     public static Stack<String> createStack() {
-
+    Stack<String> stack = new Stack<String>();
+    stack.add("Smith");
+    stack.add("Montessori");
+    stack.add("Peralta");
+    stack.add("House");
+    return stack;
     }
 
     public static void printAndEmptyStack(Stack<String> stack) {
-
+        System.out.println("Elemento arriba "+ stack.peek());
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
     }
 
     public static void main(String[] args) {
